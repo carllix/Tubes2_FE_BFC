2dd3a60c3a6cc255560ee0d9b92152acd9cb5b07
carllix <carloangkisan21@gmail.com>
2025-05-11 21:23:02 +0700

    feat: delete unused asset and file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata = {
   title: "Alchemy 2 Finder",
   description:
     "Discover how to create any element in Little Alchemy 2 using the magic of algorithms. Explore recipe paths with BFS, DFS, and Bidirectional Algorithm!",
+  icons: {
+    icon: "/star.png",
+  },
 };
 
 export default function RootLayout({
